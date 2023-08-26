a9de42ebc648e8d241bf2ef9f40b14b302820f9e
nemmtor <kacper0witas@gmail.com>
2023-08-27 01:18:06 +0200

    add versioning

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app';
 import { ConfigService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
+  const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
 
   await app.listen(port);
 }
