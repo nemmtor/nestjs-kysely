5de14b77cbf0e8f8df5aaea6758438f365b9a9b3
nemmtor <kacper0witas@gmail.com>
2023-08-30 22:30:23 +0200

    lint staged on pre-commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  endOfLine: 'lf',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'always',
+  editorconfig: true,
+};
