797ba077f01f09e4110c8edb39e60f7d9712fab6
nemmtor <kacper0witas@gmail.com>
2023-09-02 12:23:52 +0200

    chore: add sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { Logger, Module } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
+import { HttpException, Logger, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { SentryInterceptor, SentryModule } from '@travelerdev/nestjs-sentry';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 import { ConfigModule, ConfigService } from 'src/config';
@@ -23,11 +24,36 @@ import { HealthModule } from 'src/health';
         };
       },
     }),
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (cfg: ConfigService) => {
+        const environment = cfg.get('application').environment;
+        return {
+          dsn: cfg.get('sentry').dsn,
+          debug: environment === 'local',
+          environment,
+          enabled: !['ci', 'test'].includes(environment),
+        };
+      },
+      inject: [ConfigService],
+    }),
     ConfigModule,
   ],
   controllers: [],
   providers: [
     Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () =>
+        new SentryInterceptor({
+          filters: [
+            {
+              type: HttpException,
+              filter: (exception: HttpException) => 500 > exception.getStatus(),
+            },
+          ],
+        }),
+    },
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
